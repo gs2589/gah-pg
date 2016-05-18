29e2293a89b08cfb803fc6b5cfa932a6389bbeb8
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-05-18 15:29:24 -0400

    Add Pick to DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518191716) do
+ActiveRecord::Schema.define(version: 20160518192646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,8 @@ ActiveRecord::Schema.define(version: 20160518191716) do
   end
 
   create_table "prompts", force: :cascade do |t|
-    t.string "phrase"
+    t.string  "phrase"
+    t.integer "pick"
   end
 
 end
