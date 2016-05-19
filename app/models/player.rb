6074a50294f828a8324b9a439635b1d8dcc62643
robintully <mr.robintully@gmail.com>
2016-05-19 10:26:20 -0400

    added startinghand method for players

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,13 @@ class Player < ActiveRecord::Base
   has_many :games, through: :player_games
   has_many :games, foreign_key: "winner_id"
   # this winner / won games association may need to improve
+
+  def starting_hand
+    self.gifs.delete_all
+    8.times do
+     self.gifs << Gif.random
+   end 
+ end
+
+ 
 end
