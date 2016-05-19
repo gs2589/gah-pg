df1e75a94b33764a12d2aed0932b56bb3f6a3c49
robintully <mr.robintully@gmail.com>
2016-05-19 17:23:33 -0400

    added Gif tracking

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :games, only: [:new, :create, :show, :destroy]
+  resources :games, only: [:new, :create, :show, :destroy],:has_many => [:comments]
   resources :gifs, only: [:new, :create]
   resources :players, except: [:index]
   resources :player_games
   resources :prompts, only: [:new, :create]
+  resources :player_round_gifs
 end
