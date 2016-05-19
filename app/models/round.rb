3477e01d5eacee548792902bce3cdf0bf51d2d09
gs2589 <gabriel.seidner@gmail.com>
2016-05-19 13:54:38 -0400

    fixed #get_random_prompt and db associations. Round belongs to prompt and prompt has many rounds

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,12 +1,40 @@
 class Round < ActiveRecord::Base
   belongs_to :game
   has_many :player_round_gifs
-  has_one :prompt
+  belongs_to :prompt
   has_one :judge, class_name: "Player"
   has_one :winner, class_name: "Player"
   has_one :winning_gif, class_name: "Gif"
   has_many :player_round_gifs
   has_many :players, through: :player_round_gifs
   has_many :gifs, through: :player_round_gifs
+
+
+
+
+def get_random_prompt()
+
+    offset=rand(Prompt.count)
+
+    if self.game.multiple_prompt_mode?
+    offset2=rand(Prompt.count)
+      #return [Prompt.offset(offset).first, Prompt.offset(offset2).first ]
+      #this feature is currently not supported
+    else
+      offset=rand(Prompt.count)
+
+      require 'pry'
+      binding.pry
+      self.prompt=Prompt.offset(offset).first
+      self.save
+    end
+  end
+
+
+
+
+
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519153218) do
-
+ActiveRecord::Schema.define(version: 20160519174012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +39,6 @@ ActiveRecord::Schema.define(version: 20160519153218) do
   create_table "prompts", force: :cascade do |t|
     t.string  "phrase"
     t.integer "pick"
-    t.integer "round_id"
   end
 
   create_table "rounds", force: :cascade do |t|
