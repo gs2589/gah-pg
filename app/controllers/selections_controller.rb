74e2c21827537b92363713edb86daa606cd4ac8c
robintully <mr.robintully@gmail.com>
2016-05-20 16:35:19 -0400

    moved selection create back to restful convention

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -4,17 +4,16 @@ class SelectionsController < ApplicationController
   end
 
   def create
-
     # player posts selection here
     # assign player, gif, round all those ids to the selection and save to DB
-  end
-
-  def show
     round =  Round.find_by_id(params[:round_id])
     gif =  Gif.find_by_id(params[:selection][:gif_id])
     player = Player.find_by_id(session[:user_id])
     Selection.create(selected_gif_id: gif.id, player: player, round: round)
     redirect_to round.game
+  end
+
+  def show
   end
 
   def show
