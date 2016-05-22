ca39118538fb453f80948105d0e1125176a2c7b1
gs2589 <gabriel.seidner@gmail.com>
2016-05-22 17:55:56 -0400

    create new game functionality done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,9 +6,14 @@ class GamesController < ApplicationController
    ## session[:username] = params[:username]
    ##@player=Player.find_or_create_by(username: params[:username])
     
+session[:username] = params[:username]
+@player=Player.find_or_create_by(username: params[:username])
+session[:user_id] = @player.id
 
-    @game=Game.create() 
-
+@game=Game.create()
+session[:game_id]=@game.id
+@game.players << @player # shovel all the player into the game that they specify
+@player.starting_hand
 
 round1 = Round.create(game_round: 1, game: @game)
 round1.get_random_prompt
@@ -31,8 +36,16 @@ round6.get_random_prompt
 round7 = Round.create(game_round: 7, game: @game)
 round7.get_random_prompt
 
+@a=game_path(@game)
+
+
+
+ render json: {game: game_path(@game)}
+
+end
+
     
-  end
+  
     # redirect_to game_path(@game)
     # Shovel 4 players into the game
     # generate n game rounds
@@ -44,6 +57,8 @@ round7.get_random_prompt
 
 
       @game = Game.find_by_id(session[:game_id])
+
+      (redirect_to(game_path(@game)) and return) if params[:id]!=@game.id.to_s
       @round = @game.rounds.find_by(winner_id:nil)
       @prompt = @round.prompt
       @player = Player.find_by_id(session[:user_id])
