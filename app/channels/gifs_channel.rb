86f8cdecbd13dd4815dafdb3924f4610b11b00a0
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-05-23 01:54:10 -0400

    New changes for ActionCable

--- app/channels/gifs_channel.rb
+++ app/channels/gifs_channel.rb
@@ -2,4 +2,15 @@ class GifsChannel < ApplicationCable::Channel
   def subscribed
     stream_from: 'gifs'
   end
+
+  def select_gif gif
+    ActionCable.server.broadcast('gifs', 
+      gif: render_gif(gif) )
+  end
+
+  private
+  def render_gif gif
+    ApplicationController.render(partial: 'gifs/gif',
+                                 locals: {gif: gif})
+  end
 end
