0601686a00bf835c7177eb257a962709d69f2924
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-05-23 00:41:59 -0400

    Resolved conflict

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -25,55 +25,53 @@ class GamesController < ApplicationController
     round7 = Round.create(game_round: 7, game: @game,judge: Player.find_by_id(1))
     round7.get_random_prompt
   end
-    # redirect_to game_path(@game)
-    # Shovel 4 players into the game
-    # generate n game rounds
-    # Round.create x 4 (prompts and all that are generated)
-    # now the game is ready to be played through all its rounds
+  # redirect_to game_path(@game)
+  # Shovel 4 players into the game
+  # generate n game rounds
+  # Round.create x 4 (prompts and all that are generated)
+  # now the game is ready to be played through all its rounds
 
-    def show
-      @game = Game.find_by_id(params[:id])
-      @round = Round.find_by(winner_id: nil)
+  def show
+    @game = Game.find_by_id(params[:id])
+    @round = Round.find_by(winner_id: nil)
+    @prompt = @round.prompt
+    @player = Player.find_by_id(session[:user_id])
+    @game = Game.find_by_id(params[:id])
+    @round = @game.rounds.find_by(winner_id: nil)
+    # Stuff that only needs to happen if there is a round
+
+    if @round
       @prompt = @round.prompt
-      @player = Player.find_by_id(session[:user_id])
-      @game = Game.find_by_id(params[:id])
-      @round = @game.rounds.find_by(winner_id: nil)
-      # Stuff that only needs to happen if there is a round
+      # Determining the Judge
+    end
+    if @round.game_round == 1
+      @judge = Player.first
+    else
+      @judge =  Round.find_by_id((@round.id) -1).winner
+    end
 
-      if @round
-        @prompt = @round.prompt
-        # Determining the Judge
-
-      if @round.game_round == 1
-        @judge = Player.first
-      else
-        @judge =  Round.find_by_id((@round.id) -1).winner 
+    # Assigning the correct gifs
+    if @judge == @player
+      @gifs =@round.selections.map do |selection|
+        selection.gif
       end
-
-       # Assigning the correct gifs
-      if @judge == @player
-        @gifs =@round.selections.map do |selection|
-          selection.gif
-        end
-      else
-        @gifs =  @player.gifs
-      end
+    else
+      @gifs =  @player.gifs
+    end
     # end
-# Redirections
-if @round == nil
-  render '_game_over'
-elsif  @judge == @player
-  render '_show_czar'
-elsif @game.players.include?(@player)
-  render '_show_player'
-else
-  "You aren't in this game Bozo"
+    # Redirections
+    if @round == nil
+      render '_game_over'
+    elsif  @judge == @player
+      render '_show_czar'
+    elsif @game.players.include?(@player)
+      render '_show_player'
+    else
+      "You aren't in this game Bozo"
+    end
+
+    def destroy
+    end
+  end
 end
 
- end
-
- def destroy
- end
-
-
-end
