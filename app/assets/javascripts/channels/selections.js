2a03d9525a0cd9858c50923e5c3d6accd1f713c1
gs2589 <gabriel.seidner@gmail.com>
2016-05-24 19:37:27 -0400

    refactored code

--- app/assets/javascripts/channels/selections.js
+++ app/assets/javascripts/channels/selections.js
@@ -9,9 +9,6 @@ App.messages = App.cable.subscriptions.create('SelectionsChannel', {
 
     //check game id in zar player view
 
-    if (data["game"]==parseInt($('.col-md-2 .text-center h4').text().match(/[0-9 -()+]+/)[0])){
-      location.reload()
-    }
 
 
     //return $('#gifs').append(this.renderGif(data));
