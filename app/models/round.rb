cf527987d9e70526fe9ed24e6f22d6c205f575c3
gs2589 <gabriel.seidner@gmail.com>
2016-05-31 13:53:06 -0400

    refactored this a bit

--- app/models/round.rb
+++ app/models/round.rb
@@ -27,7 +27,12 @@ def get_random_prompt()
   end
 
 
-
+def self.initiate_rounds_for_game(game, number_of_rounds, judge_player)
+  number_of_rounds.times do |i|
+    Round.create(game_round: i, game: game, judge: judge_player).get_random_prompt if i==1
+    Round.create(game_round: i, game: game).get_random_prompt if i>1
+  end
+end
 
 
 
